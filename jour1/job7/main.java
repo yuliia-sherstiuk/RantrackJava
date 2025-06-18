2ccddab8faba17f6bba2c95dbdd5b2088553a266
yuliia-sherstiuk <yuliia.sherstiuk@laplateforme.io>
2025-06-18 17:26:18 +0200

    Ajouté les  job6 et job7 pour le jour1

---
+++ jour1/job7/main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0; // // Déclare une variable pour stocker la somme
+
+        // //additionner les nombres de 1 à 100
+        for (int i = 1; i <= 100; i++) {
+            sum += i; // // Ajoute i à la somme
+        }
+
+        System.out.println("The sum of the first 100 integers is: " + sum); // // Affiche le résultat
+    }
+}
